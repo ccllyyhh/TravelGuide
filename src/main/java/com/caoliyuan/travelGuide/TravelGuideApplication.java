a413e4b417a3903588e5c67eaf1e6dd9be6e0b6e
ccllyyhh <36835118+ccllyyhh@users.noreply.github.com>
2019-03-01 13:24:52 +0800

    init

---
+++ src/main/java/com/caoliyuan/travelGuide/TravelGuideApplication.java
@@ -0,0 +1,13 @@
+package com.caoliyuan.travelGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelGuideApplication.class, args);
+	}
+
+}
